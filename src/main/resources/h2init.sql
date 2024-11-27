65dd7b201eee6ac0cd858fbab38860d366f563a1
zako0001 <zako0001@stud.kea.dk>
2024-11-27 12:34:49 +0100

    Frontpage made of single project

--- src/main/resources/h2init.sql
+++ src/main/resources/h2init.sql
@@ -1,11 +1,12 @@
 CREATE TABLE project(
     project_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(20) NOT NULL,
+    title VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE sub_project(
+CREATE TABLE subproject(
     subproject_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(20) NOT NULL,
+    project_id INT NOT NULL,
     FOREIGN KEY(project_id) REFERENCES project(project_id) ON DELETE CASCADE
 );
 
@@ -13,12 +14,16 @@ CREATE TABLE task(
     task_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(20) NOT NULL,
     deadline DATETIME,
-    FOREIGN KEY(subproject_id) REFERENCES sub_project(subproject_id) ON DELETE CASCADE
+    subproject_id INT NOT NULL,
+    FOREIGN KEY(subproject_id) REFERENCES subproject(subproject_id) ON DELETE CASCADE
 );
 
-CREATE TABLE sub_task(
+CREATE TABLE subtask(
     subtask_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(20) NOT NULL,
     estimated_hours FLOAT NOT NULL,
+    task_id INT NOT NULL,
     FOREIGN KEY(task_id) REFERENCES task(task_id) ON DELETE CASCADE
 );
+
+INSERT INTO project (title) VALUES ('The project');
