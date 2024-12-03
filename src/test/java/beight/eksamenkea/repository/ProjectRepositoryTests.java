2d9cc39f7a53d876da930f1d03d4094657e625c5
zako0001 <zako0001@stud.kea.dk>
2024-12-03 19:16:23 +0100

    'Edit/change/update title' feature for project that can easily work for subproject, task, and subtask as well. Also fixed some tests for subproject and task.

--- src/test/java/beight/eksamenkea/repository/ProjectRepositoryTests.java
+++ src/test/java/beight/eksamenkea/repository/ProjectRepositoryTests.java
@@ -4,17 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
-@Rollback(true)
 //@ActiveProfiles("h2")
 public class ProjectRepositoryTests {
 
@@ -23,26 +19,34 @@ public class ProjectRepositoryTests {
 
     @Test
     void readProject() {
+        assertNull(projectRepository.readProject(2));
         assertEquals("The project", projectRepository.readProject(1).getTitle());
     }
 
     @Test
-    void createAndReadSubproject() {
-        assertThrows(DataIntegrityViolationException.class, () -> projectRepository.createSubproject(1, null));
-        assertNull(projectRepository.readSubproject(1));
-        boolean actualB = projectRepository.createSubproject(1, "The subproject");
-        //String actualS = projectRepository.readSubproject(1).getTitle(); // Bug in the test class only: Subproject is null.
-        assertTrue(actualB);
-        //assertEquals("The subproject", actualS);
+    void readSubproject() {
+        assertNull(projectRepository.readSubproject(2));
+        assertEquals("The subproject", projectRepository.readSubproject(1).getTitle());
     }
 
     @Test
-    void createAndReadTask() {
+    void readTask() {
+        assertNull(projectRepository.readTask(2));
+        assertEquals("The task", projectRepository.readTask(1).getTitle());
+    }
+
+    @Test
+    void createSubproject() {
+        assertThrows(DataIntegrityViolationException.class, () -> projectRepository.createSubproject(1, null));
+        assertThrows(DataIntegrityViolationException.class, () -> projectRepository.createSubproject(2, "test"));
+        assertTrue(projectRepository.createSubproject(1, "test"));
+    }
+
+    @Test
+    void createTask() {
         assertThrows(DataIntegrityViolationException.class, () -> projectRepository.createTask(1,null, LocalDateTime.now()));
-        assertNull(projectRepository.readTask(1));
-        projectRepository.createSubproject(1, "The subproject");
-//        boolean actualB = projectRepository.createTask(1, "test", LocalDateTime.now());
-//        assertTrue(actualB);
+        assertThrows(DataIntegrityViolationException.class, () -> projectRepository.createTask(2, "test", LocalDateTime.now()));
+        assertTrue(projectRepository.createTask(1, "test", LocalDateTime.now()));
     }
 /*
     @Test
@@ -51,4 +55,17 @@ public class ProjectRepositoryTests {
         assertThrows(DataIntegrityViolationException.class, () -> projectRepository.createSubTask(null, 2.25f)); //Failure
     }
 */
+    @Test
+    void readTitle() {
+        assertNull(projectRepository.readTitle("wrongHere", 1));
+        assertThrows(EmptyResultDataAccessException.class, () -> projectRepository.readTitle("project", 2));
+        assertEquals("The project", projectRepository.readTitle("project", 1));
+    }
+
+    @Test
+    void updateTitle() {
+        assertFalse(projectRepository.updateTitle("wrongHere", 1, "test"));
+        assertFalse(projectRepository.updateTitle("project", 2, "test"));
+        assertTrue(projectRepository.updateTitle("project", 1, "test"));
+    }
 }
